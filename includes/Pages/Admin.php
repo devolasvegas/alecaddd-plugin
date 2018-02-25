507feb9b986156c80ca308d6e326cd44430914a0
DEVON DAVIAU <devon.daviau@yahoo.ca>
2018-02-25 16:46:53 -0500

    add settingsapi class

--- includes/Pages/Admin.php
+++ includes/Pages/Admin.php
@@ -6,21 +6,30 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 class Admin extends BaseController
 {
 
+	public $settings;
+
+	public function __construct() {
+		$this->settings = new SettingsApi();
+	}
+
 	public function register() {
-		add_action('admin_menu', array($this, 'add_admin_pages'));
+//		add_action('admin_menu', array($this, 'add_admin_pages'));
+
+		$this->settings->addPages($this->pages)->register();
 	}
 
-	public function add_admin_pages() {
-		add_menu_page('Alecad Plugin', 'Alecad', 'manage_options', 'alecad_plugin',  array($this, 'admin_index'), 'dashicons-store', 110);
-	}
-
-	public function admin_index() {
-		// require template
-		require_once $this->plugin_path . 'templates/admin.php';
-	}
+//	public function add_admin_pages() {
+//		add_menu_page('Alecad Plugin', 'Alecad', 'manage_options', 'alecad_plugin',  array($this, 'admin_index'), 'dashicons-store', 110);
+//	}
+//
+//	public function admin_index() {
+//		// require template
+//		require_once $this->plugin_path . 'templates/admin.php';
+//	}
 
 }
